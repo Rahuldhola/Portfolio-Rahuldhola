be1a3376f51d7662908a0d78dd2dbcec74f9aa6c
Rahuldhola <rahuldhola222@gmail.com>
2025-07-28 11:16:22 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -29,10 +29,20 @@ function About() {
 
       <div className="about-content">
         <h2>About Me</h2>
-        <p>
-          I’m a frontend developer with over 1.5 years of experience building dynamic user interfaces using React JS.
-          I’ve built chatbot apps with real-time messaging, login/signup, status features, and real-time notifications using WebSockets.
-        </p>
+         <div className="about-lines">
+          <p className="line left">
+            I’m a frontend developer with over 1.5 years of experience.
+          </p>
+          <p className="line right">
+            I specialize in building modern UIs using React JS.
+          </p>
+          <p className="line left">
+            I’ve developed chatbot apps with real-time messaging.
+          </p>
+          <p className="line right">
+            With login/signup, status features, and WebSocket notifications.
+          </p>
+        </div>
 
         
       </div>

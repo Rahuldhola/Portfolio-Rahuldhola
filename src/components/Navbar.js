c7f84fd65c2cc2c4892853628b62cf35a0e62812
Rahuldhola <rahuldhola222@gmail.com>
2025-07-30 16:39:03 +0530

    updatee

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <a href='/'>
           <img src={profileImg} alt="Rahul Dhola" className='navbar-profile-img' />
         </a>
-        <a href="/" onClick={closeMenu} className="navbar-a" style={{ textDecoration: 'none', }}>
+        <a href="/" onClick={closeMenu} className="navbar-a" >
           Rahul
         </a>
       </div>
